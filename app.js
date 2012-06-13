837baf20595933347978e2f160003fc2b28a90ec
unknown <bay@BORISOV.intetics.com.ua>
2012-06-13 17:11:49 +0300

    - code formatting;

--- app.js
+++ app.js
@@ -9,44 +9,49 @@ Ext.application({
 
     onReady:function () {
 
-       Ext.regModel('dataModel', {
-            fields: [
-                     {name: 'errorMsg',  type: 'string'},
-                     {name: 'data',   type: 'int'}
-                    ]
-                });
+        Ext.regModel('dataModel', {
+            fields:[
+                {name:'errorMsg', type:'string'},
+                {name:'data', type:'int'}
+            ]
+        });
 
-       window.dataStore = new Ext.data.Store({
-            model: 'dataModel',
-            proxy: {
-                     type: 'ajax',
-                     url : 'http://localhost:8080/JsonServlet?q=5'
-                    },
-            autoLoad: true
-          })
-        },
+        window.dataStore = new Ext.data.Store({
+            model:'dataModel',
+            proxy:{
+                type:'ajax',
+                url:'http://localhost:8080/JsonServlet?q=5'
+            },
+            autoLoad:true
+        })
+    },
 
     launch:function () {
 
-        function renewData(){
+        function renewData() {
             window.dataStore.load();
-             };
+        }
 
-        var periodicFunction = window.setInterval(renewData,1000);
+        var periodicFunction = window.setInterval(renewData, 1000);
         var app = Ext.create('voting-charts.view.Main');
         Ext.Viewport.add(app);
 
         settlerRelativeCoordinates = {
-          setValue: function(x,y){
-              var height = window.innerHeight;
-              var width = window.innerWidth;
+            setValue:function (x, y) {
+                var height = window.innerHeight;
+                var width = window.innerWidth;
 
-              var onePercentHeight = Math.round((height/100));
-              var onePercentWidth = Math.round((width/100));
+                var onePercentHeight = Math.round((height / 100));
+                var onePercentWidth = Math.round((width / 100));
 
-              if(x==0){return myWid = y*onePercentWidth};
-              if(y==0){return myHeg = x*onePercentHeight}
-             }
-         }
+                if (x == 0) {
+                    return myWid = y * onePercentWidth
+                }
+
+                if (y == 0) {
+                    return myHeg = x * onePercentHeight
+                }
+            }
+        }
     }
 });
